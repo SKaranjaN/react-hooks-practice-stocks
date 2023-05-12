372ba59170d428485ec6cbf819ef7f5791a24400
Stephen <skaranja654@gmail.com>
2023-05-12 18:47:13 +0300

    feat: GET data feature added

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Stock from "./Stock";
 
 function StockContainer() {
+  const [stk, setStk] = useState([]);
+
+  function fetchData() {
+    fetch("http://localhost:3000/stocks")
+      .then((r) => r.json())
+      .then((data) => setStk(data));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stk.map((stockItem) => {
+        return (
+          <Stock
+            name={stockItem.name}
+            ticker={stockItem.ticker}
+            price={stockItem.price}
+          />
+        );
+      })}
     </div>
   );
 }
